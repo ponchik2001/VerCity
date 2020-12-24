8e6e3bda2281a91a0ea176cbba4cc37724cae5b5
ponchik2001 <61233993+ponchik2001@users.noreply.github.com>
2020-12-24 22:01:50 +0300

    Типо норм, но слайдеры глючат

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ prv_b = $('.right-btn'),
 nxt_b = $('.left-btn'),
 sldrInterval = 330000,
 animateTime = 1000,
-course = 1,
+course = -1,
 margin = - slideWidth;
 $('.wrap-container .latest-arrivals__item:last').clone().prependTo('.wrap-container');$('.wrap-container .latest-arrivals__item ').eq(1).clone().appendTo('.wrap-container');$('.wrap-container').css('margin-left',-slideWidth);function nxt_bSlide(){interval=window.setInterval(animate,sldrInterval)}function animate(){if(margin==-slideCount*slideWidth-slideWidth){sldr.css({'marginLeft':-slideWidth});margin=-slideWidth*2}else if(margin==0&&course==-1){sldr.css({'marginLeft':-slideWidth*slideCount});margin=-slideWidth*slideCount+slideWidth}else{margin=margin-slideWidth*(course)}sldr.animate({'marginLeft':margin},animateTime)}function sldrStop(){window.clearInterval(interval)}prv_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=-1;animate();course=course2});nxt_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=1;animate();course=course2});sldr.add(nxt_b).add(prv_b).hover(function(){sldrStop()},nxt_bSlide);nxt_bSlide()});
 
