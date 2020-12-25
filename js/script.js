6b1e7a40db2ea0f308b7bb21e71d8c36cc45e90a
ponchik2001 <61233993+ponchik2001@users.noreply.github.com>
2020-12-25 12:19:58 +0300

    Вроде всё, пусть проверит, там посмотрим

--- js/script.js
+++ js/script.js
@@ -29,15 +29,9 @@ slideWidth = $('.latest-arrivals__container').outerWidth(),
 slideCount = $('.wrap-container .latest-arrivals__item').length,
 prv_b = $('.right-btn'),
 nxt_b = $('.left-btn'),
-sldrInterval = 330000,
+sldrInterval = 40000,
 animateTime = 1000,
 course = 1,
 margin = - slideWidth;
 $('.wrap-container .latest-arrivals__item:last').clone().prependTo('.wrap-container');$('.wrap-container .latest-arrivals__item ').eq(1).clone().appendTo('.wrap-container');$('.wrap-container').css('margin-left',-slideWidth);function nxt_bSlide(){interval=window.setInterval(animate,sldrInterval)}function animate(){if(margin==-slideCount*slideWidth-slideWidth){sldr.css({'marginLeft':-slideWidth});margin=-slideWidth*2}else if(margin==0&&course==-1){sldr.css({'marginLeft':-slideWidth*slideCount});margin=-slideWidth*slideCount+slideWidth}else{margin=margin-slideWidth*(course)}sldr.animate({'marginLeft':margin},animateTime)}function sldrStop(){window.clearInterval(interval)}prv_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=-1;animate();course=course2});nxt_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=1;animate();course=course2});sldr.add(nxt_b).add(prv_b).hover(function(){sldrStop()},nxt_bSlide);nxt_bSlide()});
 
-
-function counterP(){
-    var sum = document.getElementById('counter1').innerHTML;
-    console.log(sum);
-    
-}
